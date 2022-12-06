fd14b02378cfb21f6908550b1e8903f1ff4d2f36
koozoo <meteoramtr@gmail.com>
2022-12-06 12:37:59 +0400

    create project, make main pages

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const HomePage = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Woman Shop</title>
+      </Head>
+    </>
+  );
+};
+
+export default HomePage;
